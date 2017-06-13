79cb6d6d451e344b995dd5fb9a1cbba73044a14c
0u812 <0u812@users.noreply.github.com>
2017-06-12 22:52:29 -0700

    add datapoint collector

--- zimmer/LV/generator.py
+++ zimmer/LV/generator.py
@@ -1,5 +1,6 @@
 import tellurium as te
 import numpy as np
+from math import sqrt
 
 # Load the Lotka-Volterra model
 LVModel = te.loada('''
@@ -35,21 +36,29 @@ end
 ''')
 
 class collector:
-    def __init__(self, model, selections):
+    def __init__(self, model, selections, noise_var, should_round=True):
         self.model = model
         self.selections = selections
+        self.noise_var = noise_var
+        self.should_round = should_round
+        if self.should_round:
+            self.round = round
+        else:
+            self.round = lambda x: x
 
-    def collect_selections(self):
+    def collect_selections(self, time):
         ''' Collects the selections at the current time.
         Returns 1D array.
         '''
         result = np.zeros(len(self.selections)+1)
-        result[0] = self.model.time
-        for k,s in enumerate(selections, start=1):
-            result[k] = self.model['[{}]'.format(s)]
+        result[0] = time
+        for k,s in enumerate(self.selections, start=1):
+            result[k] = self.round(self.model['[{}]'.format(s)])
+        if self.noise_var > 0:
+            result[1:] += np.random.normal(0., sqrt(noise_var), len(self.selections))
         return result
 
-    def __call__(model, seed, times, selections, noise_var, should_round=True):
+    def __call__(self, times, seed):
         ''' Collect the data points.
 
         times: list of times
@@ -66,23 +75,26 @@ class collector:
 
         length = len(times)
 
-        result = np.zeros((length,1+len(self.selections))
+        result = np.zeros((length,1+len(self.selections)))
 
         for k,t in enumerate(times):
             if t != 0:
                 self.model.resetAll()
                 self.model.integrator.seed = seed
-                self.model.simulate(0,100,int(t*10+1))
-            result[k,:] = self.collect_selections()
+                self.model.simulate(0,t,int(t*10+1))
+            result[k,:] = self.collect_selections(t)
 
         return result
 
 times = [0., 5., 10., 15., 20., 25., 30., 40., 50., 60.]
+seed = 1234
 
+c = collector(LVModel, ['A','B'], noise_var=0., should_round=True)
+datapoints = c(times, seed)
 
 # plot the traces
 LVModel.integrator = 'gillespie'
-LVModel.integrator.seed = 1234
+LVModel.integrator.seed = seed
 LVModel.integrator.variable_step_size = False
 LVModel.resetAll()
 LVModel.simulate(0,100,100)
